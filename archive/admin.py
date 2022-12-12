3024d49b529dc3e49088f12cd66eb9ef98f4279d
Al <eganalan@gmail.com>
2022-12-12 23:48:01 +0000

    Add models to models.py
    Add
    - Tune and Comment models to models.py
    
    Update
    - admin.py with Tune

--- archive/admin.py
+++ archive/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tune
 
+admin.site.register(Tune)
 # Register your models here.
--- archive/models.py
+++ archive/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+TUNE_TYPE = (
+    ('Jig', 'Jig'),
+    ('Hornpipe', 'Hornpipe'),
+    ('Reel', 'Reel'),
+    ('Polka', 'Polka'),
+    ('Setdance', 'Setdance'),
+    ('Slip Jig', 'Slip Jig'),
+    ('Slow Air', 'Slow Air'),
+    ('Barndance', 'Barndance'),
+)
 
 # Create your models here.
+
+
+class Tune(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="archive_posts", null=True
+    )
+    title = models.CharField(max_length=200, unique=True)
+    tune_type = models.CharField(
+        max_length=9, choices=TUNE_TYPE, default='Jig')
+    composer = models.CharField(max_length=200, unique=True)
+    learned_from = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='tune_like', blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Tune, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
