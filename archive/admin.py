05b7a4d7cf27110302563b7fd867f5351b2d2e4f
Al <eganalan@gmail.com>
2022-12-13 00:58:41 +0000

    Install summernote and build admin panel
    Install
    - Summernote WYSIWYG editor
    
    Build
    - Tune admin panel
    - Comment admin panel
    
    Update
    - urls.py and settings.py for Summernote

--- archive/admin.py
+++ archive/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Tune
+from .models import Tune, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Tune)
 # Register your models here.
+
+@admin.register(Tune)
+class TuneAdmin(SummernoteModelAdmin):
+    
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title', )}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    search_fields = ['name', 'email', 'body']
+    list_filter = ('approved', 'created_on')
+    actions = ['approve_comments']
+
+    def approve_comments(self, requiest, queryset):
+        queryset.update(approved=True)
