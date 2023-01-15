06b634966365033e015a3a9c3f19a719ad0bf596
Al <eganalan@gmail.com>
2023-01-15 17:48:00 +0000

    Create Views, Add basic html and styling
    Create:
    - View for tune list
    - View for single tune
    
    Add:
    - base.html
    - navbar.html
    - index.html
    - logo.png
    - basic styling in styles.css
    
    Update:
    - urls and settings with html, css, and images

--- archive/views.py
+++ archive/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Tune
 
-# Create your views here.
+
+class TuneList(generic.ListView):
+    model = Tune()
+    queryset = Tune.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
+class TuneDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Tune.objects.filter(status=1)
+        tune = get_object_or_404(queryset, slug=slug)
+        comments = tune.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if tune.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "#",
+            {
+                "tune": tune,
+                "comments": comments,
+                "liked": liked
+            },
+        )
--- tradarchive/settings.py
+++ tradarchive/settings.py
@@ -131,13 +131,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATUCFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+# STATIC_URL = '/static/'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# MEDIA_URL = '/media/'
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
